139f1c9f871562c0bd07d79b5ffc3c85a45abe3e
Batyr <b.bobetayev.97@gmail.com>
2020-11-29 15:26:57 +0100

    base set up

---
+++ src/views/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './../App.scss'
+
+const Main = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Loading...
+      </header>
+    </div>
+  )
+}
+
+export default Main
